2c68c4d9db132a16c0c1825de8306e52a8551bf8
Aman Ullah <amanix007@gmail.com>
2021-04-12 19:57:40 +0600

    working iftar time

--- js/main.js
+++ js/main.js
@@ -1,280 +1,269 @@
 var RamdanData = [
     {
-        "DATE": "2021-04-08",
-        "SEHR": "5:30AM",
-        "IFTAR": "6:30PM"
+        "DATE": "2021-04-12",
+        "SEHR": "04:22",
+        "IFTAR": "18:19"
     },
-    // {
-    //     "DATE": "09/04/2021",
-    //     "SEHR": "04:25 AM",
-    //     "IFTAR": "06:18 PM"
-    // },
-    // {
-    //     "DATE": "10/04/2021",
-    //     "SEHR": "04:24 AM",
-    //     "IFTAR": "06:19 PM"
-    // },
-    // {
-    //     "DATE": "11/04/2021",
-    //     "SEHR": "04:23 AM",
-    //     "IFTAR": "06:19 PM"
-    // },
-    // {
-    //     "DATE": "12/04/2021",
-    //     "SEHR": "04:22 AM",
-    //     "IFTAR": "06:20 PM"
-    // },
-    // {
-    //     "DATE": "13/04/2021",
-    //     "SEHR": "04:21 AM",
-    //     "IFTAR": "06:20 PM"
-    // },
-    // {
-    //     "DATE": "14/04/2021",
-    //     "SEHR": "04:20 AM",
-    //     "IFTAR": "06:20 PM"
-    // },
-    // {
-    //     "DATE": "15/04/2021",
-    //     "SEHR": "04:19 AM",
-    //     "IFTAR": "06:21 PM"
-    // },
-    // {
-    //     "DATE": "16/04/2021",
-    //     "SEHR": "04:18 AM",
-    //     "IFTAR": "06:21 PM"
-    // },
-    // {
-    //     "DATE": "17/04/2021",
-    //     "SEHR": "04:17 AM",
-    //     "IFTAR": "06:22 PM"
-    // },
-    // {
-    //     "DATE": "18/04/2021",
-    //     "SEHR": "04:16 AM",
-    //     "IFTAR": "06:22 PM"
-    // },
-    // {
-    //     "DATE": "19/04/2021",
-    //     "SEHR": "04:15 AM",
-    //     "IFTAR": "06:22 PM"
-    // },
-    // {
-    //     "DATE": "20/04/2021",
-    //     "SEHR": "04:13 AM",
-    //     "IFTAR": "06:23 PM"
-    // },
-    // {
-    //     "DATE": "21/04/2021",
-    //     "SEHR": "04:12 AM",
-    //     "IFTAR": "06:23 PM"
-    // },
-    // {
-    //     "DATE": "22/04/2021",
-    //     "SEHR": "04:11 AM",
-    //     "IFTAR": "06:24 PM"
-    // },
-    // {
-    //     "DATE": "23/04/2021",
-    //     "SEHR": "04:10 AM",
-    //     "IFTAR": "06:24 PM"
-    // },
-    // {
-    //     "DATE": "24/04/2021",
-    //     "SEHR": "04:10 AM",
-    //     "IFTAR": "06:25 PM"
-    // },
-    // {
-    //     "DATE": "25/04/2021",
-    //     "SEHR": "04:09 AM",
-    //     "IFTAR": "06:25 PM"
-    // },
-    // {
-    //     "DATE": "26/04/2021",
-    //     "SEHR": "04:08 AM",
-    //     "IFTAR": "06:25 PM"
-    // },
-    // {
-    //     "DATE": "27/04/2021",
-    //     "SEHR": "04:07 AM",
-    //     "IFTAR": "06:26 PM"
-    // },
-    // {
-    //     "DATE": "28/04/2021",
-    //     "SEHR": "04:06 AM",
-    //     "IFTAR": "06:26 PM"
-    // },
-    // {
-    //     "DATE": "29/04/2021",
-    //     "SEHR": "04:05 AM",
-    //     "IFTAR": "06:27 PM"
-    // },
-    // {
-    //     "DATE": "30/04/2021",
-    //     "SEHR": "04:04 AM",
-    //     "IFTAR": "06:27 PM"
-    // },
-    // {
-    //     "DATE": "01/05/2021",
-    //     "SEHR": "04:03 AM",
-    //     "IFTAR": "06:28 PM"
-    // },
-    // {
-    //     "DATE": "02/05/2021",
-    //     "SEHR": "04:02 AM",
-    //     "IFTAR": "06:28 PM"
-    // },
-    // {
-    //     "DATE": "03/05/2021",
-    //     "SEHR": "04:01 AM",
-    //     "IFTAR": "06:29 PM"
-    // },
-    // {
-    //     "DATE": "04/05/2021",
-    //     "SEHR": "04:00 AM",
-    //     "IFTAR": "06:29 PM"
-    // },
-    // {
-    //     "DATE": "05/05/2021",
-    //     "SEHR": "03:59 AM",
-    //     "IFTAR": "06:30 PM"
-    // },
-    // {
-    //     "DATE": "06/05/2021",
-    //     "SEHR": "03:59 AM",
-    //     "IFTAR": "06:30 PM"
-    // },
-    // {
-    //     "DATE": "07/05/2021",
-    //     "SEHR": "03:58 AM",
-    //     "IFTAR": "06:31 PM"
-    // }
+    {
+        "DATE": "2021-04-13",
+        "SEHR": "04:21",
+        "IFTAR": "18:20"
+    },
+    {
+        "DATE": "2021-04-14",
+        "SEHR": "04:20",
+        "IFTAR": "18:20"
+    },
+    {
+        "DATE": "2021-04-15",
+        "SEHR": "04:19",
+        "IFTAR": "18:20"
+    },
+    {
+        "DATE": "2021-04-16",
+        "SEHR": "04:18",
+        "IFTAR": "18:21"
+    },
+    {
+        "DATE": "2021-04-17",
+        "SEHR": "04:17",
+        "IFTAR": "18:21"
+    },
+    {
+        "DATE": "2021-04-18",
+        "SEHR": "04:16",
+        "IFTAR": "18:22"
+    },
+    {
+        "DATE": "2021-04-19",
+        "SEHR": "04:15",
+        "IFTAR": "18:22"
+    },
+    {
+        "DATE": "2021-04-20",
+        "SEHR": "04:14",
+        "IFTAR": "18:23"
+    },
+    {
+        "DATE": "2021-04-21",
+        "SEHR": "04:13",
+        "IFTAR": "18:23"
+    },
+    {
+        "DATE": "2021-04-22",
+        "SEHR": "04:12",
+        "IFTAR": "18:23"
+    },
+    {
+        "DATE": "2021-04-23",
+        "SEHR": "04:11",
+        "IFTAR": "18:24"
+    },
+    {
+        "DATE": "2021-04-24",
+        "SEHR": "04:10",
+        "IFTAR": "18:24"
+    },
+    {
+        "DATE": "2021-04-25",
+        "SEHR": "04:09",
+        "IFTAR": "18:25"
+    },
+    {
+        "DATE": "2021-04-26",
+        "SEHR": "04:08",
+        "IFTAR": "18:25"
+    },
+    {
+        "DATE": "2021-04-27",
+        "SEHR": "04:07",
+        "IFTAR": "18:26"
+    },
+    {
+        "DATE": "2021-04-28",
+        "SEHR": "04:06",
+        "IFTAR": "18:26"
+    },
+    {
+        "DATE": "2021-04-29",
+        "SEHR": "04:05",
+        "IFTAR": "18:27"
+    },
+    {
+        "DATE": "2021-04-30",
+        "SEHR": "04:04",
+        "IFTAR": "18:27"
+    },
+    {
+        "DATE": "2021-05-01",
+        "SEHR": "04:03",
+        "IFTAR": "18:27"
+    },
+    {
+        "DATE": "2021-05-02",
+        "SEHR": "04:02",
+        "IFTAR": "18:28"
+    },
+    {
+        "DATE": "2021-05-03",
+        "SEHR": "04:01",
+        "IFTAR": "18:28"
+    },
+    {
+        "DATE": "2021-05-04",
+        "SEHR": "04:01",
+        "IFTAR": "18:29"
+    },
+    {
+        "DATE": "2021-05-05",
+        "SEHR": "04:00",
+        "IFTAR": "18:29"
+    },
+    {
+        "DATE": "2021-05-06",
+        "SEHR": "03:59",
+        "IFTAR": "18:30"
+    },
+    {
+        "DATE": "2021-05-07",
+        "SEHR": "03:58",
+        "IFTAR": "18:30"
+    },
+    {
+        "DATE": "2021-05-08",
+        "SEHR": "03:57",
+        "IFTAR": "18:31"
+    },
+    {
+        "DATE": "2021-05-09",
+        "SEHR": "03:57",
+        "IFTAR": "18:31"
+    },
+    {
+        "DATE": "2021-05-10",
+        "SEHR": "03:56",
+        "IFTAR": "18:32"
+    },
+    {
+        "DATE": "2021-05-11",
+        "SEHR": "03:55",
+        "IFTAR": "18:32"
+    },
+    {
+        "DATE": "2021-05-11",
+        "SEHR": "03:54",
+        "IFTAR": "18:33"
+    }
 ];
 
 
-var bengali = ["০", "১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯"];
+var bengaliNumbers = ["০", "১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯"];
 
 
 
-//    var dm =  {
-//     "DATE": "08 April 2021",
-//     "SEHR": "04:26 AM",
-//     "IFTAR": "06:18 PM"
-//   }
-
-// $("#getting-started")
-//     .countdown("2022/01/01", function (event) {
-//         console.log('event:', event)
-//         $(this).text(
-//             event.strftime('%D days %H:%M:%S')
-//         );
-//     });
-
-// YYYY-MM-DDTHH:mm:ss.sssZ
 var timeType = [
     "আজকের ইফতার",
     "আজকের সেহেরী"
 ];
 function init() {
+    // 2011-10-05T14:48:00.000Z
 
-    let dhakaTimeDate = moment().tz("Asia/Dhaka");
+    let typeShowTypelist = ["todays_seheri", "todays_iftar", "next_days_seheri"];
+
+    let CurrentdhakaTimeDate = moment().tz("Asia/Dhaka");
+    // let CurrentdhakaTimeDate = moment("2021-04-12T18:33:00.000Z");
+    let CurrentdhakaTimeDateString = moment().format("YYYY-MM-DD");
+    console.log('CurrentdhakaTimeDateString:', CurrentdhakaTimeDateString)
+
+
+    let TodaySehriTime = "";
+    let TodayIftarTime = "";
+
+    let NextdaySehriTime = "";
+    let NextdayIftarTime = "";
+
+
     let RamadanNumber = 0;
-    var CurrentTimeType = "SEHR";
-    var SelectedTimeType = timeType[0];
-    var SelectedTime = moment().format('YYYY-MM-DD')
-    var TodaySehriTime = "";
-    var TodayIftarTime = "";
-    var TodayDate = moment().format("YYYY-MM-DD")
-    // console.log('TodayDate:', TodayDate)
-    for (var i = 0; i < RamdanData.length; i++) {
-        var data = RamdanData[i];
-        if (data.DATE === TodayDate) {
-            // TodaySehriTime = `${data.DATE}T${data.SEHR}.000Z`;
-            // TodayIftarDateTime = `${data.DATE}T${data.IFTAR}.000Z`;
-            TodaySehriTime = `${data.SEHR}.000Z`;
-            TodayIftarTime = `${data.IFTAR}.000Z`;
 
+
+
+    for (let i = 0; i < RamdanData.length; i++) {
+        let data = RamdanData[i];
+        let nextDaydata = RamdanData[i + 1];
+        if (data.DATE === CurrentdhakaTimeDateString) {
+
+            TodaySehriTime = `${data.DATE}T${data.SEHR}:00.000Z`;
+            TodayIftarTime = `${data.DATE}T${data.IFTAR}:00.000Z`;
+
+            NextdaySehriTime = `${nextDaydata.DATE}T${nextDaydata.SEHR}:00.000Z`;
+            NextdayIftarTime = `${nextDaydata.DATE}T${nextDaydata.IFTAR}:00.000Z`;
             RamadanNumber = i;
         }
     }
 
-    
-    //   console.log('dhakaTimeDate:', dhakaTimeDate.format("h:mma"))
-    
-    console.log('TodayIftarTime:', TodayIftarTime)
-    
-    
-    // if (dhakaTimeDate.isAfter(moment("06:20PM", "h:mma"), "minute")) {
-    if (dhakaTimeDate.isAfter(moment(TodaySehriTime, "HH:mm:ss.sssZ"), "minute") && dhakaTimeDate.isBefore(moment(TodayIftarTime, "HH:mm:ss.sssZ"), "minute")) {
 
-        console.log("its iftar Time next")
-    }else if(dhakaTimeDate.isAfter(moment(TodayIftarTime, "HH:mm:ss.sssZ"), "minute") &&  dhakaTimeDate.isBefore(moment(TodaySehriTime, "HH:mm:ss.sssZ"), "minute")){
-        console.log("its sehr Time next")
-        
+
+
+    function getNextTimeType() {
+        let nextDataType = "todays_seheri";
+
+        if (moment(CurrentdhakaTimeDate).isBefore(TodaySehriTime, 'second') && moment(CurrentdhakaTimeDate).isBefore(TodayIftarTime, 'second')) {
+            nextDataType = "todays_seheri";
+        }
+
+        if (moment(CurrentdhakaTimeDate).isAfter(TodaySehriTime, 'second') && moment(CurrentdhakaTimeDate).isBefore(TodayIftarTime, 'second')) {
+            nextDataType = "todays_iftar";
+        }
+
+        if (moment(CurrentdhakaTimeDate).isBefore(NextdaySehriTime, 'second')) {
+            nextDataType = "next_days_seheri";
+        }
+
+        return nextDataType;
     }
 
-    
 
-    setInterval(function () {
-        // console.log("Hello");
-        document.getElementById("event-type").innerText = SelectedTimeType;
-        document.getElementById("event-time").innerText = SelectedTime;
 
-    }, 1000);
+    function replaceEnglishToBanglaDigit(string) {
+        let stringArray = string.split("");
+        stringArray[0] = bengaliNumbers[parseInt(stringArray[0])];
+        stringArray[1] = bengaliNumbers[parseInt(stringArray[1])];
+        // stringArray[2] = `<span class="colon">ঃ</span>`;
+        stringArray[2] = `<span class="colon">:</span></span>`;
+        stringArray[3] = bengaliNumbers[parseInt(stringArray[3])];
+        stringArray[4] = bengaliNumbers[parseInt(stringArray[4])];
+
+        return stringArray.join('')
+
+    }
+
+
+    function injectDateToDom() {
+        let nextDataType = getNextTimeType();
+        console.log('nextDataType:', nextDataType)
+
+        if (nextDataType === "todays_seheri") {
+            document.getElementById("event-type").innerHTML = "আজকের সেহেরী";
+            document.getElementById("event-time").innerHTML = replaceEnglishToBanglaDigit(moment.utc(TodaySehriTime).format("hh:mm"));
+        } else if (nextDataType === "todays_iftar") {
+            document.getElementById("event-type").innerHTML = "আজকের ইফতার";
+            document.getElementById("event-time").innerHTML = replaceEnglishToBanglaDigit(moment.utc(TodayIftarTime).format("hh:mm"));
+        } else if (nextDataType === "next_days_seheri") {
+            document.getElementById("event-type").innerHTML = "আগামীকালের সেহেরী";
+            document.getElementById("event-time").innerHTML = replaceEnglishToBanglaDigit(moment.utc(NextdaySehriTime).format("hh:mm"));
+
+        }
+    }
+
+    injectDateToDom();
+
+    setInterval(() => {
+        injectDateToDom();
+    }, 10000);
+
+
 }
 
 
-// const imageScript = (request, ad, date) =>{
-//     document.addEventListener('DOMContentLoaded', function (event) {
-
-
-//         // var imageSrc = 'https://dev-panel.green-red.com/uploads/5fc4eca4173f8.jpg';
-//         var apiEndPoint = '${process.env.CLICK_EVENT_URL}';
-//         var payload = {
-//             placeId: '${request.placeId}',
-//             propertyId:'${request.propertyId}',
-//             cookie: '${request.cookie}',
-//             requestId: '${ad.campaignId}|${ad.adGroupId}|${ad.id}',
-//             date:'${date}',
-//             actionType:''
-//         };
-
-//         var iframeNode = document.createElement('iframe');
-//         iframeNode.src = ad.htmlsrc;
-//         iframeNode.alt = title;
-//         iframeNode.style.width = '100%';
-//         iframeNode.style.height = '100%';
-
-//         var wrapper = document.createElement('div');
-//         wrapper.className = 'adv-area-wrapper';  
-//         wrapper.appendChild(iframeNode);
-//         var x = document.getElementsByTagName('BODY')[0];
-//         x.appendChild(wrapper);
-//         x.onclick = function () {
-//             var xhr = new XMLHttpRequest();
-//             xhr.withCredentials = true;
-//             xhr.open('POST', apiEndPoint);
-
-//             xhr.setRequestHeader('Content-Type', 'application/json');
-//             xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-//             payload.actionType = "click";
-//             xhr.send(JSON.stringify(payload));
-
-//             xhr.addEventListener('readystatechange', function () {
-//                 if (this.readyState === 4) {
-//                     const { response, status } = this;
-//                     console.log({ response, status });
-//                     if (status === 201) {
-//                         return response;
-//                     }
-//                     return false;
-//                 }
-//             });
-//         };
-//     })
-//     return(``)};
-
-
-// imageScript();
 
 window.onload = init;
